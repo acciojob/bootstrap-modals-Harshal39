bb03563181f0651d1ab5d86a7a560a1234753af6
Harshal Bhute <114854206+Harshal39@users.noreply.github.com>
2024-04-05 17:23:54 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,5 @@
-//your code here
+const openModalButton = document.getElementById('open-modal');
+openModalButton.addEventListener('click', function() {
+  const modal = new bootstrap.Modal(document.getElementById('newton-modal'));
+  modal.show();
+});
